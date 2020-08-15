9959b368720345b76f99aeb332529c1f3e5f0b2f
njs04210 <pearlnkim@gmail.com>
2020-08-15 16:22:04 +0900

    정적인 파일의 서비스

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.get('*', function (request, response, next) {
@@ -23,7 +24,11 @@ app.get('/', function (request, response) {
   var description = 'Hello, Node.js';
   var list = template.list(request.list);
   var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}`,
+    `
+    <h2>${title}</h2>
+    ${description}
+    <img src="/images/beach.jpeg" style="width:300px; margin-top:10px; display:block;">
+    `,
     `<a href="/create">create</a>`
   );
   response.send(html);
